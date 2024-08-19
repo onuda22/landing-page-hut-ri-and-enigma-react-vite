e822e0a5f3bc5d29720baa748abecd0119a970f9
randy <randytjiioe95@gmail.com>
2024-08-19 19:15:49 +0700

    feat/event

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Event from "./component/hutRI/Event";
 import Hero from "./component/hutRI/Hero";
 
 function App() {
   return (
     <div className="bg-gradient-to-tr from-[#ff9a9e] via-[#fad0c4] to-[#fad0c4] bg-opacity-10 rounded-lg">
       <Hero />
+      <Event />
     </div>
   );
   // <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
