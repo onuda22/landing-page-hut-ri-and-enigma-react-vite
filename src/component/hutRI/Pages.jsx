756b8e0e21454616b4794feb23c1f7fd4fc8dfa7
randy <randytjiioe95@gmail.com>
2024-08-19 22:24:09 +0700

    feat/gallery

--- src/component/hutRI/Pages.jsx
+++ src/component/hutRI/Pages.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Hero from './Hero'
-import Event from './Event'
+import React from "react";
+import Hero from "./Hero";
+import Event from "./Event";
+import Gallery from "./Gallery";
 
 export default function Pages() {
   return (
     <div>
       <Hero />
       <Event />
+      <Gallery />
     </div>
-  )
+  );
 }
