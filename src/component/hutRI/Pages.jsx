9a31322931170ca4beb2f9f12291e4100fd56788
Muhammad Amirul <mirulid@gmail.com>
2024-08-19 21:58:48 +0700

    merge Pages

--- src/component/hutRI/Pages.jsx
+++ src/component/hutRI/Pages.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero'
+import Event from './Event'
 
 export default function Pages() {
   return (
     <div>
       <Hero />
+      <Event />
     </div>
   )
 }
