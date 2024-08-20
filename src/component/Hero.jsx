66e1698b44ce28f1ae904fb2362e53478a09d2fa
Muhammad Amirul <mirulid@gmail.com>
2024-08-20 07:53:58 +0700

    component follow

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -5,17 +5,13 @@ import { CarouselComp } from './CarouselComp'
 import ReactDOM from 'react-dom';
 export default function Hero() {
   return (
-    <nav class="p-2 border-gray-200 dark:bg-gray-900">
-      {/* <div>
-
-      // </div> */}
       <div className='w-full'>
         <Header />
-        {/* <Main /> */}
+        <Main />
         <Follow />
-        {/* <CarouselComp /> */}
+        <CarouselComp />
       </div>
-    </nav>
+
   )
 }
 
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import Header from '../component/Header'
-import Main from '../component/Main'
-import CardTeam from '../component/CardTeam'
-import Meet from '../component/Meet'
-import Footer from '../component/Footer'
-import { Splash } from '../component/Splash'
+import React from "react";
+import Header from "../component/Header";
+import Main from "../component/Main";
+import CardTeam from "../component/CardTeam";
+import Meet from "../component/Meet";
+import Footer from "../component/Footer";
+import { Splash } from "../component/Splash";
+// import { CarouselComp } from "../component/CarouselComp";
+import Follow from "../component/Follow";
 
 export default function Hero({ onButtonClick }) {
   return (
-    <div className='w-full'>
-        {/* <Splash /> */}
-      <Header onButtonClick={onButtonClick}/>
+    <div className="w-full">
+      {/* <Splash /> */}
+      <Header onButtonClick={onButtonClick} />
       <Main />
+      <Follow />
+      {/* <CarouselComp /> */}
       <Meet />
       <CardTeam />
       <Footer />
-      
     </div>
-  )
+  );
 }
